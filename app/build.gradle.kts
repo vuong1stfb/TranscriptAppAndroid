342ef0f6780f7272608672acb93e337ab35e02cd
vuong1stfb <vuong1st.fb@gmail.com>
2025-09-21 00:09:11 +0700

    Escape ampersand in split button label

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 android {
     namespace = "com.example.transcriptapp"
     compileSdk = 34
-    buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "com.example.transcriptapp"
